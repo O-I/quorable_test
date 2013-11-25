36617aea0825f32074fbadfa21c4fe0a980ed9da
Rahul Horé <hore.rahul@gmail.com>
2013-11-25 15:09:48 -0500

    Changes quorable to quorable_test

--- main.rb
+++ main.rb
@@ -12,7 +12,8 @@ topic = 'data structures'
 topic = normalize(topic)
 
 doc = Nokogiri::HTML.parse(open("#{base_url}/#{topic}"))
-questions = doc.css('.question_link').map { |q| q.children.last.children.last.text }
+questions = doc.css('.link_text').map(&:text)
+# expanded_texts = doc.css('.expanded_q_text').map { }
 links = doc.css('.question_link').map { |link| link['href'] }
 
 p questions, links
